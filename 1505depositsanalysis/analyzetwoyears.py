f9fbf3dfa66532e4810ae501f1a650447df2ccc6
fmartinez2 <fmartinez2@opportunity.DUNCLLC.COM>
2016-09-15 05:15:50 -0500

    add all years data, raw
    add raw data for all below 5,000

---
+++ 1505depositsanalysis/analyzetwoyears.py
@@ -0,0 +1,26 @@
+# import twoyearssummed.txt and read it in an a string array. 
+# two years summary was created by cat'ing out the files and then 
+# sed'ing out words like "Department" and "False" in a lazy way because
+# Linux SysAdmin Life
+
+expense=[]
+data_twoyears=open('twoyearssummed.txt','r')
+type(data_twoyear)
+data_twoyears.read().decode('utf8')
+print data
+#print expense
+
+#print data_twoyears
+
+#print type(expenses)
+#print expenses[0]
+
+#for row in data_twoyears:
+    #row=row.replace(',','')
+    #row=row.replace('\'','')
+#    print list(eval('row'))
+#    type(row)	
+#    expense.append(float(row))
+#    row.recompile('\'[^[0-9]$]*')
+#    print expense[0]
+#    print expense[3]
---
+++ 1505depositsanalysis/contcat.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+for i in `seq 21 87`; do
+ cat 1505deposists2.$i >> 2010to2015.txt
+done
+
+
+
