0139ba78acf919b0224998d0b174ced90d45d09f
Freddy Martinez <fremartinez@expedia.com>
2017-07-01 14:02:03 -0500

    only use address, drop redundant columns

--- biketickets/cleanup.py
+++ biketickets/cleanup.py
@@ -1,7 +1,9 @@
 import csv
 
 with open('BikeTickets.csv', 'rb') as csvfile:
+     i=1
      for line in csvfile.readlines():
          line.strip('\n')
          if line and (not line.isspace()):
              print(line.strip('\n'))
+             i+=1
